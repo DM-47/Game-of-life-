3662e22a20ecb14ac6962cc9d282c8fa1d8590e4
DM-47 <106817122+DM-47@users.noreply.github.com>
2022-06-05 17:58:31 +0400

    Add files via upload
    2rd

---
+++ Grass.js
@@ -0,0 +1,29 @@
+class Grass extends LivingCreature {
+	constructor(x,y){
+	super(x,y)
+        this.multiply = 10
+        
+		
+        
+	}
+
+	
+
+	mul(){
+        this.multiply++
+
+        let arr = this.chooseCell(0) 
+        let emptyCell = random(arr)
+		if(this.multiply >= 4 && emptyCell){
+
+            let x = emptyCell[0]
+            let y = emptyCell[1]
+            matrix[y][x] = 1
+            var gr = new Grass(x,y)
+            grassArr.push(gr)
+            everything.push(gr)
+            this.multiply = 0 
+            
+		}
+	}
+}
